b17035387edf779ddbf55dfd8bab7c8a269ff5cd
abhimanyu012 <abhimanyucode2021@gmail.com>
2026-01-08 19:24:58 +0530

    Refactor lead storage and rendering logic in index.js

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-
+let myLeads = [];
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
-let myLeads = ["Abhimanyu ", "Aman ", "Avy"];
 let list = document.getElementById("list-el");
 
 inputBtn.addEventListener("click", () => {
-  for (let i = 0; i < myLeads.length; i++) {
-    // list.innerHTML += "<li>" + myLeads[i] + "</li>";
-    const li = document.createElement("li")
-    li.textContent = myLeads[i]
-    list.append(li)
-}
+  if (inputEl.value !== "") {
+    myLeads.push(inputEl.value);
+    renderList();
+  }
+  inputEl.value = "";
 });
+
+function renderList() {
+  list.innerHTML = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = myLeads[i];
+    list.append(li);
+  }
+}
